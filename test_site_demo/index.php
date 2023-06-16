b8db241ba71a06eb2dd6f8f7557acbf60ea66b50
Morozovvvv <77523805+Morozovvvv@users.noreply.github.com>
2023-06-16 20:55:09 +0300

    Add files via upload

---
+++ test_site_demo/index.php
@@ -0,0 +1,15 @@
+<html>
+    <link rel="shortcut icon" href="/images/logo.ico">
+    <center><img src="/images/logo.ico" height="190px"></img></center>
+    <head><link rel="stylesheet" href="styles/styles.css"><title>Авторизация</title></head> 
+    <body>
+        <form method="POST" action="/scripts/code.php">
+                <p align = "center">Авторизация</p>
+            <table border="0" align="center">
+                <tr><td>Логин: <input type="text" name="login"/></td></tr>
+                <tr><td>Пароль: <input size="20" type="password" name="pass"></td></tr>
+                <td align="center"><button name="loginBtn" type="sumbit" value="logins">Войти</button></td>
+            </table>
+        </form>
+    </body> 
+</html>
